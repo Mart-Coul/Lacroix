e6d77eae4106f62f7ca69b51e87125330f260931
Martin Coulombe <mart.coulombe@gmail.com>
2013-05-13 21:53:13 -0400

    Added datetime functionnality ala Facebook style " x time ago "

--- module/Lacroix/src/Lacroix/Entity/ProductionLine.php
+++ module/Lacroix/src/Lacroix/Entity/ProductionLine.php
@@ -96,10 +96,25 @@ class ProductionLine {
     if (!$reading) {
       return $this->getTranslator()->translate('N/A');
     };
+    
+    return $this->formatDateXTimeAgo($reading->getCreatedAt());
+  }
+  
+  public function formatDateXTimeAgo($date) {
+    $stf = 0;
+	$cur_time = time();
+	$diff = $cur_time - $date;
+	$phrase = array('second','minute','hour','day','week','month','year','decade');
+	$length = array(1,60,3600,86400,604800,2630880,31570560,315705600);
 
-    $datetime = new \DateTime();
-    $datetime->setTimestamp($reading->getCreatedAt());
-    return $datetime->format($format);
+	for($i = sizeof($length)-1; ($i >=0) && (($no =  $diff/$length[$i]) <= 1); $i--); if($i < 0) $i=0; $_time = $cur_time  -($diff%$length[$i]);
+	$no = floor($no); 
+	if($no > 1) $phrase[$i] .='s'; 
+	$value=sprintf("%d %s ",$no,$phrase[$i]);
+
+	if(($stf == 1)&&($i >= 1)&&(($cur_tm-$_time) > 0)) $value .= time_ago($_time);
+
+	return $value.' ago ';
   }
 
   public function getProductivityOptions() {

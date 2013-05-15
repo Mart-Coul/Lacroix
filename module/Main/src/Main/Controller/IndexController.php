e9d2d0a1165eebead12a1b9be75978452be6deb7
Konstantin Burnaev <bkon@bkon.ru>
2013-05-15 11:13:57 +0400

    Mobile users are now redirected to the statistics page.

--- module/Main/src/Main/Controller/IndexController.php
+++ module/Main/src/Main/Controller/IndexController.php
@@ -6,6 +6,11 @@ use Main\Entity\UserRole;
 class IndexController extends ApplicationController {
   public function indexAction() {
     $user = $this->getIdentity();
+
+    if (preg_match('/iPhone|mobile/i', $_SERVER['HTTP_USER_AGENT'])) {
+      return $this->redirect()->toRoute('statistics');
+    };
+
     if ($user->hasRole(UserRole::ROLE_ADMIN)) {
       return $this->redirect()->toRoute('configuration');
     } else {
